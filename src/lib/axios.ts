8800b97763b846bc4b1ae906abbb2219ce1ff212
Théo Barreto Silva <barretotheo25@gmail.com>
2025-06-15 22:54:00 -0300

    upload link backend deploy

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://backendepimanager-production.up.railway.app/",
   headers: {
     "Content-Type": "application/json",
   },
